2dbc83b57332e57909f1c103cef2c9b60f7e48b0
Brandi Apetsi <brandi@Brandis-MacBook.local>
2019-04-24 22:19:04 -0400

    working on login page

--- better-friends/src/components/LogIn.js
+++ better-friends/src/components/LogIn.js
@@ -1,11 +1,40 @@
-import React from 'react'
+import React, {Component} from 'react'
+import axios from 'axios'
 
-const LogIn = () => {
-    return (
-        <>
-            <h1>Log In</h1>
-        </>
-    )
+class LogIn extends Component {
+    state = {
+        username: '',
+        password: ''
+    }
+
+    render() {
+        return (
+            <>
+                <h1>Log In</h1>
+
+                <form>
+                    <input
+                        type='string'
+                        name='username'
+                        placeholder='Username'
+                        value={this.state.username}
+                        onChange={this.handleChange}
+                    />
+                
+                    <input 
+                        type='string'
+                        name='password'
+                        placeholder='Password'
+                        value={this.state.password}
+                        onChange={this.handleChange}
+                    /> 
+                 
+                    <button> Log In </button>
+                </form>
+            </>
+        )
+    }
+    
 }
 
 export default LogIn
